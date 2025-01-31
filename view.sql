24c7786d15a596dbcc5df9d8558148be6bf6ce3f
krapottke <krapottke_git@icloud.com>
2025-01-31 15:16:15 +0100

    view angepasst

--- view.sql
+++ view.sql
@@ -1,4 +1,6 @@
 create view PROJEKT_VIEW_MITARBEITER AS
-select ROLLE, count(*) as "Mitarbeiter Anzahl", avg(GEHALT)
+select ROLLE, count(*) as "Mitarbeiter Anzahl", round(avg(GEHALT)) as Durschnittsgehalt
 FROM PROJEKT_MITARBEITER
 group by ROLLE;
+
+drop view PROJEKT_VIEW_MITARBEITER;
